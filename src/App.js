5560597e6df0efac2ee438ccc3de21143128c383
Marten Hernebring <marten.hernebring@gmail.com>
2022-02-15 11:48:18 +0100

    Added enzyme 17, specified using extend-expect, changed link from react to epochtimes sweden

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Välkommen!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://epochtimes.se/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Epoch Times Sverige
         </a>
       </header>
     </div>
