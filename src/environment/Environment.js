1a70cd29e6fea315743dd86917daf615a5a371f6
Marten Hernebring <marten.hernebring@gmail.com>
2022-02-21 15:00:18 +0100

    Changed article id

--- src/environment/Environment.js
+++ src/environment/Environment.js
@@ -1,5 +1,5 @@
 const Environment = {
   BASE_URL: 'http://localhost:8181/v1/articles',
-  ARTICLE: '/inrikes/2022/ekonomi/5971'
+  ARTICLE: '/inrikes/2022/ekonomi/2836'
 }
 export default Environment
